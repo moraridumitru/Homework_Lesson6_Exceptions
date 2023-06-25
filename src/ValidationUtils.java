2751ecd1f91775772585e0a67a178f3f3e32c422
moraridumitru <moraridumitru1@gmail.com>
2023-06-25 12:06:47 +0300

    Homework Lesson6 Exceptions

--- src/ValidationUtils.java
+++ src/ValidationUtils.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class ValidationUtils {
+public class ValidationUtils {
+    public static void validateLoginPassword(String login, String password, String confirmPassword) throws WrongLoginException, WrongPasswordException {
+        if (!login.matches("[a-zA-Z0-9_]+")) {           // [a-zA-Z0-9_] reges - регулярные выражения Java . https://javarush.com/groups/posts/regulyarnye-vyrazheniya-v-java
+            throw new WrongLoginException("Login should contain only Latin letters, numbers, and underscores.");
+        }
+
+        if (login.length() >= 20) {
+            throw new WrongLoginException("Login length should be less than 20 characters.");
+        }
+
+        if (!password.matches("[a-zA-Z0-9_]+")) {
+            throw new WrongPasswordException("Password should contain only Latin letters, numbers, and underscores.");
+        }
+
+        if (password.length() >= 20) {
+            throw new WrongPasswordException("Password length should be less than 20 characters.");
+        }
+
+        if (!password.equals(confirmPassword)) {
+            throw new WrongPasswordException("Password and confirm password do not match.");
+        }
+    }
+
+    public static void main(String[] args) {
+        String login = "valid_login";
+        String password = "valid_password";
+        String confirmPassword = "valid_password";
+
+        try {
+            validateLoginPassword(login, password, confirmPassword);
+            System.out.println("Login and password are valid.");
+        } catch (WrongLoginException e) {
+            System.out.println("Invalid login: " + e.getMessage());
+        } catch (WrongPasswordException e) {
+            System.out.println("Invalid password: " + e.getMessage());
+        }
+    }
 }
--- src/WrongLoginException.java
+++ src/WrongLoginException.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class WrongLoginException {
+
+/**Создать статический метод который принимает на вход три параметра: login, password и confirmPassword.
+ Login должен содержать только латинские буквы, цифры и знак подчеркивания.
+ Длина login должна быть меньше 20 символов. Если login не соответствует этим требованиям, необходимо выбросить WrongLoginException.
+ Password должен содержать только латинские буквы, цифры и знак подчеркивания.
+ Длина password должна быть меньше 20 символов. Также password и confirmPassword должны быть равны. Использоввать исключения**/
+class WrongLoginException extends Exception {   //Класс Exception — это встроенный класс, представляющий исключения. Он является частью стандартной библиотеки Java и автоматически доступен во всех программах Java.
+    public WrongLoginException(String message) {
+        super(message);                       // При вызове метода super() с аргументам message мы фактически вызываем конструктор
+    }
 }
+
+class WrongPasswordException extends Exception {
+    public WrongPasswordException(String message) {
+        super(message);
+    }
+}
+
